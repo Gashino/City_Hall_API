5d0aab49c69321b4c1b9aee8f879244b63667926
Agustin Aused <agus.aused@gmail.com>
2024-05-20 16:35:24 -0300

    feat: add login and change password endpoints

--- src/main/java/cityHallAPI/dev/controllers/UserControler.java
+++ src/main/java/cityHallAPI/dev/controllers/UserControler.java
@@ -1,23 +1,23 @@
 package cityHallAPI.dev.controllers;
 
+import cityHallAPI.dev.dtos.UserDto;
 import cityHallAPI.dev.exceptions.UserException;
 import cityHallAPI.dev.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person")
 public class UserControler {
 
     @Autowired
     private IUserService userService;
 
     @PostMapping("/register?dni={dni}&email={email}")
-    public ResponseEntity<Object> createUser(String dni, String email) {
+    public ResponseEntity<Object> createUser(@RequestParam("dni") String dni,
+                                             @RequestParam("email") String email) {
         try {
             userService.addUser(dni, email);
             //devuelve un estado 201 (creado)
@@ -27,9 +27,38 @@ public class UserControler {
             //devuelve un mensaje de error y un estado 400
             ResponseEntity<Object> respuesta = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
             return respuesta;
-
         }
 
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<Object> login(@RequestBody String email,
+                                        @RequestBody String password) {
+        try {
+            UserDto uDto = userService.login(email, password);
+            //devuelve un estado 200 (ok)
+            return new ResponseEntity<>(uDto,HttpStatus.OK);
+        } catch (UserException e) {
+            e.printStackTrace();
+            //devuelve un mensaje de error y un estado 400
+            ResponseEntity<Object> respuesta = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            return respuesta;
+        }
+    }
+
+    @PutMapping("/person/changePassword")
+    public ResponseEntity<Object> changePassword(@RequestBody String email,
+                                                @RequestBody String newPassword) {
+        try {
+            userService.changePassword(email, newPassword);
+            //devuelve un estado 200 (ok)
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (UserException e) {
+            e.printStackTrace();
+            //devuelve un mensaje de error y un estado 400
+            ResponseEntity<Object> respuesta = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            return respuesta;
+        }
     }
 
 
